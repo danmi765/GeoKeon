c0ac592cf017f69ea4cd4901b0c986e91ccc5ad7
TaeSeong Park <404hunter@hanmail.net>
2018-04-02 21:26:05 +0900

    파일트리정리/헤더,푸터import로직추가(웹서버에서구동해야됨)

---
+++ script/main.js
@@ -0,0 +1,16 @@
+
+/* header,  footer 삽입 */
+// var header = "<div data-role='header' id='main_header'><ul><li><a href='#intro_page'>소개</a></li><li><a href='#design_page'>디자인</a></li><li><a href='#comm_page'>커뮤니티</a></li></ul></div>";
+//
+// var footer = "<div data-role='footer' id='main_footer'>FOOTER</div>";
+//
+// $(".page_box").append(footer);
+// $(".page_box").prepend(header);
+$(window).load(function(){
+    $( 'header' ).load( 'header.html', function() {
+        console.log( 'header.html' );
+    });
+    $( 'footer' ).load( 'footer.html', function() {
+        console.log( 'footer.html' );
+    });
+})
