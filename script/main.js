be503bf479f6dbe62814328192c4f85849dba829
hkb4002@gamil.com <hkb4002@gmail.com>
2018-04-05 22:30:17 +0900

    04-06-22:30

--- script/main.js
+++ script/main.js
@@ -23,3 +23,48 @@ $(window).load(function(){
 
 
 })
+
+
+/* 배너 */
+
+var interval;
+var curIndex = 0;
+var imgCnt = 0;
+
+imgCnt = $(".main_banner_inner > li > img").length;
+$(".main_banner_inner> li").eq(curIndex).css("opacity", 1).css("z-index", 2);
+
+function fadeInBanner(){
+
+    var preIndex;
+    var nextIndex;
+    preIndex = curIndex;
+    ++curIndex;
+
+    if(curIndex == imgCnt){
+        curIndex = nextIndex = 0;
+    }else{
+        nextIndex = curIndex;
+    }
+
+    $(".main_banner_inner> li").eq(preIndex).css("z-index", 1).stop().animate({"opacity" : 0}, 500, function(){ $(this).css("z-index", 0) });
+
+    $(".main_banner_inner> li").eq(nextIndex).css("z-index", 2).stop().animate({"opacity" : 1}, 500, function(){});
+
+
+} // fadeInBanner() End
+
+
+ // 배너시작
+interval = setInterval(fadeInBanner, 2000);
+
+ // 마우스오버 시 배너멈춤
+$(".main_banner_inner").hover(function(){
+
+    clearInterval(interval);
+ // 마우스 내리면 시작
+}, function(){
+
+    interval = setInterval(fadeInBanner, 2000);
+
+});
