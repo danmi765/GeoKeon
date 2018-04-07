e8431fbe7796c4a8abd50910b65a630552433c9b
hkb4002@gamil.com <hkb4002@gmail.com>
2018-04-07 19:37:33 +0900

    fadeInOutBanner finish, slideBanner modi

--- script/slideBanner.js
+++ script/slideBanner.js
@@ -5,14 +5,30 @@
    ■■■■■■■■■■■■■■■■ */
 var interval;
 var curPosi = 0; // 현재 위치
+var nextPosi; // 다음 위치
 var curImg = 0; // 총 이미지 수
 var direction = 1; // 1 : 정방향,  -1 : 역방향
+var btnNo; // 버튼의 인덱스
 
 curImg = $(".banner_inner > li").length;
 
+// 첫 로딩 시 첫번째 하단배너버튼 표시
+$(".banner_btn > li").eq(0).css("background-image","url(img/slideBtnOver.png)");
+
 function slideBanner(){
 
-    var nextPosi; // 다음 위치
+    // 하단배너버튼 표시제거
+    $(".banner_btn > li").removeAttr("style");
+
+    // 배너인덱스에 따라 하단버튼 인덱스 지정
+    switch(curPosi){
+        case 0 : btnNo = 0; break;
+        case -100 : btnNo = 1; break;
+        case -200 : btnNo = 2; break;
+        case -300 : btnNo = 3; break;
+        case -400 : btnNo = 4; break;
+    }
+
 
     // 정방향일 경우
     if( direction == 1 ){
@@ -20,12 +36,15 @@ function slideBanner(){
         $(".banner_inner").css("left", curPosi + "%").stop().animate({"left" : nextPosi + "%"}, 3000, function(){ $(this).css("left", curPosi + "%")});
         curPosi = curPosi - 100 ;
 
+        $(".banner_btn > li").eq(btnNo+1).css("background-image","url(img/slideBtnOver.png)");
+
     // 역방향일 경우
     }else if( direction == -1 ){
         nextPosi = curPosi + 100;
         $(".banner_inner").css("left", curPosi + "%").stop().animate({"left" : nextPosi + "%"}, 3000, function(){ $(this).css("left", nextPosi + "%")});
         curPosi = curPosi + 100 ;
 
+        $(".banner_btn > li").eq(btnNo-1).css("background-image","url(img/slideBtnOver.png)");
 
     }
 
@@ -48,3 +67,33 @@ $(".banner_inner").hover(function(){
 }, function(){
     interval = setInterval(slideBanner, 7000);
 });
+
+
+// 하단버튼 마우스오버 시 해당버튼 이미지변경
+$(".banner_btn > li").hover(function(){
+
+    clearInterval(interval); // 배너멈춤
+
+    btnNo = $(this).index(); // 선택된 버튼인덱스 추출
+
+    $(".banner_btn > li").not(this).removeAttr("style");
+    $(this).css("background-image","url(img/slideBtnOver.png)");
+
+    $(".banner_inner").css("left", btnNo*-100 + "%").stop().animate({"left" : btnNo*-100 + "%"}, 3000, function(){ $(this).css("left", btnNo*-100 + "%")});
+
+// 마우스 내리면 모든 스타일 제거
+}, function(){
+     $(".banner_btn > li").removeAttr("style");
+
+});
+
+
+
+
+
+
+
+
+
+
+
