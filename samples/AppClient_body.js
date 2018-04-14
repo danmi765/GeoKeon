275056fc2f63e52ec78b012a6de5b2280be2083a
TaeSeong Park <404hunter@hanmail.net>
2018-04-15 00:33:02 +0900

    new branch geoseong

---
+++ samples/AppClient_body.js
@@ -0,0 +1,15 @@
+exports.domdefault = `<body>
+    <div class="middleScreen">
+        <p id="p1">This is Default</p>
+    </div>
+    <script
+</body>
+</html>`
+
+exports.dombody = `<body>
+    <div class="middleScreen">
+        <p id="p1">Random text</p>
+    </div>
+    <script src="./consoletest.js"></script>
+</body>
+</html>`
