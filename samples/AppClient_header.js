060e8a0881420ed3dab2d976b249a95d88bb7e43
TaeSeong Park <404hunter@hanmail.net>
2018-04-15 00:31:31 +0900

    Revert "mysql 코드 추가"
    This reverts commit d05513ca8ba7b2f0856bdf39a6a6804a8c475639.

---
+++ samples/AppClient_header.js
@@ -0,0 +1,18 @@
+exports.domdefault = `<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>appclient header default</title>
+</head>
+`;
+
+exports.domheader = `<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>appclient header styled</title>
+    <link rel="stylesheet" type="text/css" href="AppClientStyle.css">
+</head>
+`;
