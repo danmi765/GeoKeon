97ddd188caee6351fef174835d9d1e072c5f8dc8
hkb4002@gamil.com <hkb4002@gmail.com>
2018-04-21 15:20:37 +0900

    main_sub_menu / design_page

--- pages/script/main.js
+++ pages/script/main.js
@@ -1,12 +1,18 @@
 
+// 서브메뉴
+$(".main_menu > li > a, .sub_menu").hover(function(e){
+    $(".main_header").stop().animate({"height" : "175px"}, 400, function(){});
+    $(".sub_menu").removeClass("clocking");
+}, function(){
+    $(".main_header").stop().animate({"height" : "70px"}, 400, function(){});
+    $(".sub_menu").addClass("clocking");
+});
+
+
+
 
 
 $(window).load(function(){
-
-
-
-
-
 
 });
 
