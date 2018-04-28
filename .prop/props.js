8a64f63205e81ec870dd98e6ddda6551a145a1b8
hkb4002@gamil.com <hkb4002@gmail.com>
2018-04-28 16:59:26 +0900

    구조변경

--- .prop/props.js
+++ .prop/props.js
@@ -2,8 +2,8 @@ module.exports.db = {
     mysql: {
         connectionLimit: 1000,
         host: 'localhost',
-        user: 'geoseong',
-        password: '1234',
+        user: 'root',
+        password: 'qwert',
         port: '3306',
         database: 'GK2018'
     }
