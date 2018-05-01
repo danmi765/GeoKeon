4ccb2d3193843532f1b9bc39baaa118194de14ee
hkb4002@gamil.com <hkb4002@gmail.com>
2018-05-02 03:09:01 +0900

    게시판기능구현1

--- routes/commboard.js
+++ routes/commboard.js
@@ -8,18 +8,18 @@ const router = express.Router();
 const commBoardController = require('../models/commboard');
 
 /** 게시판 리스트 보기 **/
-router.get('/comm', commBoardController.list);
+router.get('/comm/:commName', commBoardController.list);
 /** 게시판 글 보기 **/
-router.get('/comm_view/:commId', commBoardController.getComm);
+router.get('/comm_view/:commName&:commId', commBoardController.getComm);
 /** 게시판 글쓰기 페이지 **/
-router.get('/comm_write', commBoardController.writePage);
+router.get('/comm_write/:commName', commBoardController.writePage);
 /** 게시판 글쓰기 **/
-// router.post('/comm_write', commBoardController.write);
+router.post('/comm_write/:commName', commBoardController.write);
 /** 게시판 글수정하기 페이지 **/
-router.get('/comm_modify/:commId', commBoardController.modifyPage);
+router.get('/comm_modify/:commName&:commId', commBoardController.modifyPage);
 /** 게시판 글수정하기 **/
-// router.put('/comm_modify/:commId', commBoardController.modify);
+router.post('/comm_modify/:commName&:commId', commBoardController.modify);
 /** 게시판 글삭제하기 **/
-router.delete('/comm_remove/:commId', commBoardController.remove);
+router.post('/comm_remove/:commName&:commId', commBoardController.remove);
 
 module.exports = router;
